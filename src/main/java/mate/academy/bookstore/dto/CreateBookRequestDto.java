6b55f7f09d3a9a47645fb5583df2054d90bd5dbc
Volodymyr Shashkov <finansist33333324@gmail.com>
2024-04-01 10:25:35 +0300

    after-review

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package mate.academy.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -8,12 +9,16 @@ import lombok.Data;
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @NotEmpty
     private String title;
     @NotNull
+    @NotEmpty
     private String author;
     @NotNull
+    @NotEmpty
     private String isbn;
     @NotNull
+    @NotEmpty
     @Min(0)
     private BigDecimal price;
     private String description;
