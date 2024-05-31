c59f11bf7db5b11674d955f7b308d626be59c807
Volodymyr Shashkov <finansist33333324@gmail.com>
2024-05-31 11:59:21 +0300

    update2

--- src/test/java/mate/academy/bookstore/BookStoreApplicationTests.java
+++ src/test/java/mate/academy/bookstore/BookStoreApplicationTests.java
@@ -1,11 +1,11 @@
 package mate.academy.bookstore;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookStoreApplicationTests {
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 }
