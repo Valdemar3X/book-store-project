1c9408bbb7965ea9c4a098b4f8e3db47c211ef53
Volodymyr Shashkov <finansist33333324@gmail.com>
2024-05-31 11:55:49 +0300

    update

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -2,8 +2,10 @@ package mate.academy.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "mate.academy.bookstore.repository")
 public class BookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
